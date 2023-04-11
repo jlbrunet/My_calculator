0bf9d0b29b37ffb7c55324abc29c30ac5774bc97
Julie Brunet <juliebrunet21@msn.com>
2023-04-11 17:13:37 +0200

    stimulus en cours, le premier boutton fonctionne

--- app/javascript/controllers/calculation_controller.js
+++ app/javascript/controllers/calculation_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calculation"
 export default class extends Controller {
+  static targets = ["result"]
   connect() {
+
+  }
+
+  on() {
+    this.resultTarget.innerText = "The calculator is ONNN"
   }
 }
