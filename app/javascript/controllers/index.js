c43ab1fca572d43c05fd2b37a451278084404c88
Julie Brunet <juliebrunet21@msn.com>
2023-04-11 16:47:19 +0200

    design ok

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import CalculationController from "./calculation_controller"
+application.register("calculation", CalculationController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
